c36d927d428903df5e8665ae295331cad13633d1
Ballestero Letizía P <letiziapaolaballestero@gmail.com>
2022-05-15 14:30:40 -0300

    navbar AHORA SI ME ANDA REACT

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//import './index.css';
+
+/* comento lo que estoy probando 
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+*/ 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <React.StrictMode> <App/></React.StrictMode>);
+ 
+
 reportWebVitals();
