e470e31199b75cabcf33e0bb3b5b6dc91a4c8263
Cath Gaitan <catu.gaitan@gmail.com>
2022-05-14 23:34:18 -0300

    bebe de BS, principio de index

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,29 @@
-console.log("asdasasdasd");
+const express = require('express');
+const morgan = require('morgan');
+const router = express.Router(); 
+
+//Inicialization buen dia
+const app=express();
+
+//Settigs
+app.set('port',5000); //definir puertos
+
+//Middlewares (funciones que se ejecutan que se envia una peticion al servidor)
+app.use(morgan('dev')); //muestra por consola las peticiones que van llegando
+
+//Routes
+app.get('/',(req,res)=>{ //puedo tenerlo en otro archivo e importarlo
+    res.send('esto anda, creo');
+})
+
+
+//Start the server
+app.listen(app.get('port'),()=>{ //se ejecuta al iniciar el server
+    console.log('Server on port: ',app.get('port')); 
+}); 
+
+
+
+
+
+
