bea113de6f3f4ee11c29f2c23d3bc95e82a189c4
Lara <larafons94@gmail.com>
2022-05-17 20:57:47 -0300

    anda el registrar :)))))

--- backend/dataBase/dataBase.js
+++ backend/dataBase/dataBase.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: 'vacunassist',
     port: process.env.DB_PORT
 });
 
